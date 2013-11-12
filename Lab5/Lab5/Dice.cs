f2a98337196172e68f52aa1d9e0e7d8cae8c661d
ajenshireman <awshireman@pstcc.edu>
2013-11-12 15:11:51 -0500

    Added skeleton for game logic. Dice holds array.

--- Lab5/Lab5/Dice.cs
+++ Lab5/Lab5/Dice.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/**
+ * Dice.cs
+ * Author: Ajen Shireman
+ * 
+ * Class for simulating a group of dice.
+ * 
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Random;
@@ -8,28 +16,60 @@ namespace Lab5
 {
     class Dice
     {
-        private int sides; // Number of sides on the die
-        private Random roller; // RNG for rolling the die
+        private int    numDice;   // Number of dice in the set
+        private int    sides;     // Number of sides on each die
+        private int[]  results    // Result of the roll
+        private Random roller;    // RNG for rolling the die
 
         /* Constructors */
-        // Creates a die with 6 sides
-        public Dice () : this(6)
+        // Create a new group of dice
+        public Dice ( int numDice, int sides )
+        {
+            this.sides = sides;
+            this.numDice = numDice;
+            roller = new Random();
+        }
+
+        // Create a group of dice with 6 sides
+        public Dice ( int numDice )
+            : this(numDice, 6)
+        {
+
+        }
+
+        // Create a single die with 6 sides
+        public Dice () : this(1, 6)
         {
             
         }
 
-        // Creates a die with any number of sides
-        public Dice ( int sides )
-        {
-            this.sides = sides;
-            roller = new Random();
-        }
-
         /* Methods */
         // Roll the bones
-        public int roll ()
+        public void roll ()
         {
-            return roller.Next();
+            for ( int i = 0; i < numDice; i++ )
+            {
+                results[i] = roller.Next(1, sides);
+            }
+        }
+
+        // Return the result as an array with each separate roll
+        public int[] getRoll () 
+        {
+            int[] resultsCopy = new int[numDice];
+            results.CopyTo(resultsCopy, 0);
+            return resultsCopy; 
+        }
+
+        // Only return the total
+        public int getRollTotal ()
+        {
+            int total = 0;
+            for ( int i = 0; i < numDice; i++ ) 
+            {
+                total += results[i];
+            }
+            return total;
         }
     }
 }
--- Lab5/Lab5/Form1.cs
+++ Lab5/Lab5/Form1.cs
@@ -11,9 +11,113 @@ namespace Lab5
 {
     public partial class Form1 : Form
     {
+        /* Variable Decarations */
+        private Dice dice;        // Array containing 2 Dice objects, used to generate rolls
+        private int  point;       // Value of first roll
+        private int  currentRoll; // Value of most recent roll
+
         public Form1 ()
         {
             InitializeComponent();
+            dice = new Dice(2, 6);
+        }
+
+        /* Event Handlers */
+
+
+        /* Game Logic */
+        // Start a new game
+        private void start ()
+        {
+            // hide the start button
+
+            // enable the roll button
+
+            // set the point value to 0 and make sure it is hidden
+            point = 0;
+
+        }
+
+        // Roll the dice
+        private void roll ()
+        {
+            // roll the dice
+            dice.roll();
+
+            // the result as an array
+            int[] results = dice.getRoll();
+
+            // show the appropriate image
+
+            // get the total and display
+            currentRoll = dice.getRollTotal();
+
+            // caculate the result
+            determineResult();
+        }
+
+        // Determine the reslut of a throw
+        private void determineResult ()
+        {
+            // first throw
+            if ( point <= 0 )
+            {
+                switch ( currentRoll )
+                {
+                    case 7:
+                    case 11:
+                        // Win!
+
+                        break;
+                    case 2:
+                    case 3:
+                    case 12:
+                        // Loose
+
+                        break;
+                    default:
+                        // set point and let player continue
+
+                        break;
+                }
+            }
+            else
+            {
+                // player tries to match point before rolling a 7
+                if ( currentRoll == point )
+                {
+                    // win
+
+                }
+                else if ( currentRoll == 7 )
+                {
+                    // Loose
+
+                }
+                else
+                {
+                    // Continue
+
+                }
+            }
+        }
+
+        // Player has won
+        private void playerWin ()
+        {
+            // Show winning message
+
+            // Reset the game
+
+        }
+
+        // Player has lost
+        private void playerLoose ()
+        {
+            // Show Loosing message
+
+            // reset the game
+
         }
     }
 }

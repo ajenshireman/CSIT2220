a6395c269c1f70e797183b94c2d6514595dd528d
ajenshireman <ajenshireman@gmail.com>
2013-11-17 01:41:31 -0500

    js to reveal .details and .extras

--- Lab6/site/Scripts/script.js
+++ Lab6/site/Scripts/script.js
@@ -4,4 +4,13 @@
         e.stopPropagation();
         $(this).closest('.package').find('.extras').slideToggle();
     });
+
+    $('.package-name').wrapInner('<a href="#"></a>');
+    
+    $('.package-name').click(function(e){
+        e.preventDefault();
+        e.stopPropagation();
+        $(this).closest('.package').find('.details').slideToggle();
+    });
+    
 });

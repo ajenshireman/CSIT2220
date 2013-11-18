c0131f8674c424f0472ca5ddeb9e5830bdf3cec0
ajenshireman <ajenshireman@gmail.com>
2013-11-18 10:02:22 -0500

    Re-enabled extras hiding

--- Lab6/site/Scripts/script.js
+++ Lab6/site/Scripts/script.js
@@ -1,17 +1,7 @@
 ﻿$('document').ready(function () {
-    /*
     $('.btn-extras').click(function(e){
         e.preventDefault();
         e.stopPropagation();
         $(this).closest('.package').find('.extras').slideToggle();
     });
-
-    $('.package-name').wrapInner('<a href="#"></a>');
-    
-    $('.package-name').click(function(e){
-        e.preventDefault();
-        e.stopPropagation();
-        $(this).closest('.package').find('.details').slideToggle();
-    });
-    */
 });

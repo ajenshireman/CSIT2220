16acffb1a4fbf1f8474b462c141e7efb991ce768
ajenshireman <awshireman@pstcc.edu>
2013-11-25 16:16:40 -0500

    added the rest of the package info

--- Lab6/site2/Default.aspx.cs
+++ Lab6/site2/Default.aspx.cs
@@ -9,9 +9,16 @@ public partial class _Default : System.Web.UI.Page
 {
     /* Packages and Prices */
     // Devil's Courthouse
-    private static Extra dcaw1 = new Extra("Climbing Instruction", 100m);
-    private static Extra dcaw2 = new Extra("Equipment Rental", 40m, Extra.PER_DAY);
-    private static Package dcaw = new Package("evil’s Courthouse Adventure Weekend", "An action packed three days weekend spent camping, rock climbing, and rappelling at Devil’s Courthouse, North Carolina. This getaway for novices and expert alike. Climbing instructor is available to beginners at an optional low price. Camping equipment rental is also available.", 350m, new Extra[]{dcaw1, dcaw2});
+    private static Package dcaw = new Package("evil’s Courthouse Adventure Weekend", "An action packed three days weekend spent camping, rock climbing, and rappelling at Devil’s Courthouse, North Carolina. This getaway for novices and expert alike. Climbing instructor is available to beginners at an optional low price. Camping equipment rental is also available.", 350m, new Extra[]{new Extra("Climbing Instruction", 100m), new Extra("Equipment Rental", 40m, Extra.PER_DAY)});
+
+    // Scuba Bahama
+    private static Package sb = new Package("Scuba Bahama", " week-long cruise to Bahama with three day with three days of scuba diving. Those with prior experience may dive right in, while beginners should choose to take the optional, but very affordable lesson.", 1000m, new Extra[]{new Extra("SCUBA Instruction", 100m)});
+
+    // Sky Dive Colorado
+    private static Package sdc = new Package("Sky Dive Colorado", "Four thrilling days with expert sky diving instructors in Colorado Springs. For lodging, you may choose either the Wilderness Lodge or Luxury Inn.", 400m, new Extra[] { new Extra("Wilderness Lodge", 65m, Extra.PER_DAY), new Extra("Luxury Inn", 120, Extra.PER_DAY) });
+
+    // Baron Cliff
+    private static Package bc = new Package("Baron Cliff", "Eight days spent hiking and exploring caves in the Barron Cliff Wilderness , in Tennessee. Camping equipment rental is available.", 700m, new Extra[] { new Extra("Equipment Rental", 40m, Extra.PER_DAY) });
 
     protected void Page_Load(object sender, EventArgs e)
     {

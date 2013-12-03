d4f4fd4b9b015f817c1b1101a79502c5c1288248
ajenshireman <awshireman@pstcc.edu>
2013-12-03 17:10:58 -0500

    Script appends the price and per day

--- Lab6/site/Scripts/script.js
+++ Lab6/site/Scripts/script.js
@@ -1,7 +1,20 @@
 ﻿$('document').ready(function () {
-    $('.btn-extras').click(function(e){
+    $('.price').each(function () {
+        $(this).append(': $' + $(this).attr('data-price') + ' per person');
+    });
+
+    $('.perDay').each(function(){
+        $(this).append(' per day');
+    });
+
+    $('.btn-extras').click(function (e) {
         e.preventDefault();
         e.stopPropagation();
         $(this).closest('.package').find('.extras').slideToggle();
+    });
+
+    $('.btn-book').click(function (e) {
+        e.preventDefault();
+        var packageID = $(this).closest('.package').attr('data-packageID');
     });
 });
